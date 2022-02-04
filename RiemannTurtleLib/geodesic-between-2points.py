0855ee4d3ca2fab0c91a8dd1a67dda98d9324c74
cgodin <christope.godin@inria.fr>
2022-02-04 19:04:25 +0100

    - First incomplete implementation of LineTo in a Riemannian space. Jacobian implemented. Needs to implement Jacobian with swapped columns to avoid instabilities

--- RiemannTurtleLib/geodesic-between-2points.py
+++ RiemannTurtleLib/geodesic-between-2points.py
@@ -1,16 +1,19 @@
 """
-    Classes and functions for manipulating a Riemannian Turtle in LP-y
+    Computing the Geodesics between two points in a Riemannian space.
 
     Author: C. Godin, Inria
-    Date: Jan. 2021
+    Date: Jan. 2021-2022
     Lab: RDP ENS de Lyon, Mosaic Inria Team
 
-    scipy.linalg.solveh_banded: https://docs.scipy.org/doc/scipy/reference/generated/scipy.linalg.solveh_banded.html
+    Implementation of the algorithm described in on (Makeawa, 1996)
 
 """
+
 import numpy as np
 
-from scipy.linalg import solve_banded
+#from scipy.linalg import solve_banded # works for symmetric or hermitian matrices
+
+from sparse.linalg import spsolve      # sparse matrix solver
 
 
 # A curve on a parametric surface defined explicitly
@@ -38,3 +41,145 @@ def InitialCurve(surf,K, A, B):
 
     intial_curve = ExplicitCurveByEndPoints(surf, A, B, X)
     return
+
+def h(s, k):
+    ''' s = array of curvilinear abscissa
+    returns the kth distance between the consecutive points
+    - precondition: 0 < k <= m
+    '''
+    return s[k] - s[k-1]
+
+def omega(k, j, Xk, RCSk):
+    '''
+    Xk is the kth state vector [u,v,p,q] on the current trajectory
+    RCSk are the kth Christoffel symbols on the current trajectory
+    '''
+    pk = Xk[2]
+    qk = Xk[3]
+    a = RCSk[j,0,0]
+    b = RCSk[j,0,1]
+    return pk*a+qk*b
+
+def theta(k, j, Xk, RCSk):
+    '''
+    Xk, Xkp are the kth and (k-1)th state vector [u,v,p,q] on the current trajectory
+    RCSk are the kth Christoffel symbols on the current trajectory
+    '''
+    pk = Xk[2]
+    qk = Xk[3]
+    a = RCSk[j,0,1]
+    b = RCSk[j,1,1]
+    return pk*a+qk*b
+
+def A(s, k, j, Xk, RCSk):
+    '''
+
+    '''
+    A0 = np.array([-h(s, k)/2.,-1,0]))
+    A1 = np.array([0,-h(s, k)/2.,0,-1])
+    A2 = np.array([h(s, k)*omega(k-1,0)-1, h(s, k)*theta(k-1,0), 0, 0])
+    A3 = np.array([h(s, k)*omega(k-1,1)  , h(s, k)*theta(k-1,1)-1, 0, 0])
+
+    return np.array([A0,A1,A2,A3])
+
+
+def B(s, k, j, Xk, RCSk):
+    '''
+
+    '''
+    B0 = np.array([-h(s, k) / 2., 1, 0])
+    B1 = np.array([0, -h(s, k) / 2., 0, 1])
+    B2 = np.array([h(s, k) * omega(k, 0) + 1, h(s, k) * theta(k, 0), 0, 0])
+    B3 = np.array([h(s, k) * omega(k, 1),     h(s, k) * theta(k, 1) + 1, 0, 0])
+
+    return np.array([B0,B1,B2,B3])
+
+def Bfirst(s, k, j, Xk, RCSk):
+    '''
+    corresponds to B0
+    '''
+    B0 = np.array([1,0,0,0])
+    B1 = np.array([0,1,0,0])
+
+    return np.array([B0,B1])
+
+def Aswapped(s, k, j, Xk, RCSk)):
+
+def Bswapped(s, k, j, Xk, RCSk)):
+
+
+def Alast(s, k, j, Xk, RCSk):
+    '''
+    corresponds to Am
+    '''
+    A0 = np.array([0,0,1,0])
+    A1 = np.array([0,0,0,1])
+
+    return np.array([A0,A1])
+
+
+def build_jacobian_csc(m):
+    '''
+    Builds the jacobian as a sparse matrix in csc format (list of (row,col) indexes of non-null entries)
+    '''
+
+    row = [0,1]  # list of row indexes of non-null elements
+    col = [0,1]  # corresponding list of row indexes of non-null elements.
+    dat = [1,1]  # data array contains non-null elements of the jacobian matrix (corresponds to blocks Ak and Bk)
+                 # The lists are initialized by non-null elements of matrix B0
+
+    # fills the jacobian matrix with non-null elements from Ak and Bk k = 1 .. m-1
+    for k in range(1,m):
+        for j in range(4):
+            # indexes present in the blocks Ak, Bk:
+            h  = 4*(k-1) + j + 2 # line index in the jacobian
+            c  = 4*(k-1) + j     # col index is non-null
+            c2 = 4*(k) + j       # second column on the same line that is also non-null for this j
+
+            d  = A(s, k, j, Xk, RCSk)
+            d2 = B(s, k+1, j, Xk, RCSk)
+
+            # add two entries on line h corresponding to non-null j entries in both Ak and Bk
+            row.append(h)
+            col.append(c)
+            dat.append(d)
+
+            row.append(h)
+            col.append(c2)
+            dat.append(d2)
+
+    # in the end add the 2 non-null elements of array Am
+    row.append(4*m-4)
+    col.append(4*m-2)
+    dat.append(1)
+
+    row.append(4*m-3)
+    col.append(4*m-1)
+    dat.append(1)
+
+    return csc_matrix((dat, (row, col)), shape=(4*m, 4*m))
+
+
+def compute_DeltaX(res_vec):
+    '''
+    Computes the path increment on the current path to lead to the genodesic path
+    res_mat = matrix of residuals at step k
+    res_vec = column vector of residuals
+    '''
+
+    # 1. Compute the set of matrixes Aswapped_k, B_swapped_k
+
+    A_array = [Aswapped(...) for k in range(1,m)]
+    B_array = [Bswapped(...) for k in range(1,m)]
+
+    # Computes the residuals jacobian matrix from ... in a csc format
+    j_mat = build_jacobian_csc()
+
+    # solves:    delta_X . jmat = res_vec  in delta_X
+    delta_X = spsolve(j_mat, res_vec)
+
+    return delta_X
+
+
+
+
